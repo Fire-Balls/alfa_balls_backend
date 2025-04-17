01b4fedd0a44c29df7960dde45daca2e24bc4e62
Yaroglek <jkl7589@gmail.com>
2025-04-17 19:56:44 +0500

    Spring initial commit

---
+++ src/main/java/org/fireballs/alfaballs/AlfaBallsApplication.java
@@ -0,0 +1,13 @@
+package org.fireballs.alfaballs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfaBallsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlfaBallsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fireballs/alfaballs/AlfaBallsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fireballs.alfaballs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfaBallsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
