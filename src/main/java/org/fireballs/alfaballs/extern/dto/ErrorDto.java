5f4553a336e47668ca1f19b2849f7ca4eb08b6fe
Yaroglek <jkl7589@gmail.com>
2025-06-13 05:02:56 +0500

    Minor fix for merge

---
+++ src/main/java/org/fireballs/alfaballs/extern/dto/ErrorDto.java
@@ -0,0 +1,16 @@
+package org.fireballs.alfaballs.extern.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.hateoas.RepresentationModel;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class ErrorDto extends RepresentationModel<ErrorDto> {
+    @NotBlank(message = "Error message must not be blank")
+    private String message;
+}
+
+
